c97a3aac33658c93eaa4e2690df6485d72c526b6
Marina Titova <una.ladrona@gmail.com>
2015-12-13 19:43:08 +0300

    Rename MediaPlayerService song into track

---
+++ MuSeek/app/src/main/java/com/letb/museek/Fragments/PlayerFragment.java
@@ -0,0 +1,100 @@
+package com.letb.museek.Fragments;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.letb.museek.Fragments.PlaylistFragment;
+import com.letb.museek.Models.Track.Track;
+import com.letb.museek.Services.MediaPlayerService;
+import com.letb.museek.R;
+
+import java.util.ArrayList;
+
+/**
+ * Created by marina.titova on 13.12.15.
+ */
+public class PlayerFragment extends Fragment implements PlaylistFragment.OnTrackSelectedListener {
+
+    public static final String TRACK_LIST = "TRACK_LIST";
+
+    private Context appContext = null;
+    private MediaPlayerService mediaPlayerService;
+    private Intent  playIntent = null;
+    private boolean mediaPlayerBound   = false;
+    private ArrayList<Track> trackList = null;
+
+    public PlayerFragment() {}
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_player, container, false);
+        if (getArguments() != null) {
+            trackList = (ArrayList<Track>) getArguments().getSerializable(TRACK_LIST);
+        }
+        return view;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getActivity() != null)
+            appContext = getActivity().getApplicationContext();
+
+//        trackList = (ArrayList<Track>) getIntent().getExtras().getSerializable(PlaylistFragment.TRACK_LIST);
+//        PlaylistFragment playlistFragment = new PlaylistFragment();
+//        playlistFragment.setArguments(getIntent().getExtras());
+//        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+//        ft.add(android.R.id.content, playlistFragment);
+//        ft.commit();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (playIntent == null) {
+            playIntent = new Intent(appContext, MediaPlayerService.class);
+            appContext.bindService(playIntent, musicConnection, Context.BIND_AUTO_CREATE);
+            appContext.startService(playIntent);
+        }
+    }
+
+    @Override
+    public void onTrackSelected(String title) {
+        Toast.makeText(appContext, title, Toast.LENGTH_SHORT).show();
+        mediaPlayerService.setTrack(Integer.parseInt(view.getTag().toString()));
+        mediaPlayerService.playTrack();
+    }
+
+    public void trackPicked(View view) {
+        // todo: change view get tag to smth useful
+
+    }
+
+//    TODO: Когда это происходит?
+    private ServiceConnection musicConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            MediaPlayerService.Binder binder = (MediaPlayerService.Binder) service;
+            mediaPlayerService = binder.getService();
+            mediaPlayerService.setTrackList(trackList);
+            mediaPlayerBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            mediaPlayerBound = false;
+        }
+    };
+}
--- MuSeek/app/src/main/java/com/letb/museek/Services/MediaPlayerService.java
+++ MuSeek/app/src/main/java/com/letb/museek/Services/MediaPlayerService.java
@@ -1,98 +1,98 @@
-//package com.letb.museek.Services;
-//
-//import android.app.Service;
-//import android.content.Intent;
-//import android.media.AudioManager;
-//import android.media.MediaPlayer;
-//import android.os.IBinder;
-//import android.os.PowerManager;
-//import android.support.annotation.Nullable;
-//import android.util.Log;
-//
-//import com.letb.museek.Models.Track.Track;
-//
-//import java.util.List;
-//
-///**
-// * Created by marina.titova on 13.12.15.
-// */
-//public class MediaPlayerService
-//        extends Service
-//        implements MediaPlayer.OnPreparedListener,
-//                    MediaPlayer.OnErrorListener,
-//                    MediaPlayer.OnCompletionListener {
-//
-//    private final IBinder binder        = new Binder();
-//    private MediaPlayer mediaPlayer;
-//    private List<Track> songList    = null;
-//    private int currentSongIndex = 0;
-//
-//    public class Binder extends android.os.Binder {
-//        public MediaPlayerService getService() {
-//            return MediaPlayerService.this;
-//        }
-//    }
-//
-//    @Nullable
-//    @Override
-//    public IBinder onBind(Intent intent) {
-//        return binder;
-//    }
-//
-//    @Override
-//    public boolean onUnbind(Intent intent){
-//        mediaPlayer.stop();
-//        mediaPlayer.release();
-//        return false;
-//    }
-//
-//    public void onCreate() {
-//        super.onCreate();
-//        currentSongIndex = 0;
-//        mediaPlayer = new MediaPlayer();
-//        initMediaPlayer();
-//    }
-//
-//    public void initMediaPlayer() {
-//        mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
-//        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-//        mediaPlayer.setOnPreparedListener(this);
-//        mediaPlayer.setOnCompletionListener(this);
-//        mediaPlayer.setOnErrorListener(this);
-//    }
-//
-//    public void setSongList(List<Track> songs) {
-//        songList = songs;
-//    }
-//
-//    public void setSong(int songId) {
-//        currentSongIndex = songId;
-//    }
-//
-//    public void playSong() {
-//        mediaPlayer.reset();
-//        Track song = songList.get(currentSongIndex);
-//        String  url = song.getUrl();
-//
-//        try {
-//            mediaPlayer.setDataSource(url);
-//            mediaPlayer.prepareAsync();
-//        }
-//        catch(Exception e){
-//            Log.e("MUSIC SERVICE", "Error setting data source", e);
-//        }
-//    }
-//
-//    @Override
-//    public void onPrepared(MediaPlayer player) {
-//        player.start();
-//    }
-//
-//    @Override
-//    public void onCompletion(MediaPlayer player) {}
-//
-//    @Override
-//    public boolean onError(MediaPlayer player, int what, int extra) {
-//        return false;
-//    }
-//}
+package com.letb.museek.Services;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.os.PowerManager;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.letb.museek.Models.Track.Track;
+
+import java.util.List;
+
+/**
+ * Created by marina.titova on 13.12.15.
+ */
+public class MediaPlayerService
+        extends Service
+        implements MediaPlayer.OnPreparedListener,
+                    MediaPlayer.OnErrorListener,
+                    MediaPlayer.OnCompletionListener {
+
+    private final IBinder binder    = new Binder();
+    private MediaPlayer mediaPlayer = null;
+    private List<Track> trackList   = null;
+    private int currentTrackIndex   = 0;
+
+    public class Binder extends android.os.Binder {
+        public MediaPlayerService getService() {
+            return MediaPlayerService.this;
+        }
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent){
+        mediaPlayer.stop();
+        mediaPlayer.release();
+        return false;
+    }
+
+    public void onCreate() {
+        super.onCreate();
+        currentTrackIndex = 0;
+        mediaPlayer = new MediaPlayer();
+        initMediaPlayer();
+    }
+
+    public void initMediaPlayer() {
+        mediaPlayer.setWakeMode(getApplicationContext(), PowerManager.PARTIAL_WAKE_LOCK);
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnErrorListener(this);
+    }
+
+    public void setTrackList(List<Track> tracks) {
+        trackList = tracks;
+    }
+
+    public void setTrack(int trackIndex) {
+        currentTrackIndex = trackIndex;
+    }
+
+    public void playTrack() {
+        mediaPlayer.reset();
+        Track song = trackList.get(currentTrackIndex);
+        String url = song.getData().getmTrack();
+
+        try {
+            mediaPlayer.setDataSource(url);
+            mediaPlayer.prepareAsync();
+        }
+        catch(Exception e){
+            Log.e("MUSIC SERVICE", "Error setting data source", e);
+        }
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer player) {
+        player.start();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer player) {}
+
+    @Override
+    public boolean onError(MediaPlayer player, int what, int extra) {
+        return false;
+    }
+}
