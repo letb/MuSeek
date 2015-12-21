54932a5cc25db1560dde5a714c253bcd104022b0
Eugene <eugene.07@list.ru>
2015-12-22 01:11:39 +0300

    New logo

--- MuSeek/app/src/main/java/com/letb/museek/SplashActivity.java
+++ MuSeek/app/src/main/java/com/letb/museek/SplashActivity.java
@@ -57,7 +57,7 @@ public class SplashActivity extends BaseSpiceActivity {
         TokenHolder.setData(event.getData().getAccessToken(), event.getData().getExpiresIn());
         UserInformer.showMessage(SplashActivity.this, event.getData().getAccessToken());
 //        TODO: For Test
-        requestTrack("6318312UUl5", "listen");
+        requestTrack("11635570PMIz", "listen");
 //        requestTopTracks(2, 1, "en");
     }
 
