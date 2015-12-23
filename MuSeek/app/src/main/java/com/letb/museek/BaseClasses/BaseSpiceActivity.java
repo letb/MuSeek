fddda6fcdb58a540e053707ab3c18599fe7cc2bf
Marina Titova <una.ladrona@gmail.com>
2015-12-23 17:47:22 +0300

    Add top navigation (does not work)

--- MuSeek/app/src/main/java/com/letb/museek/BaseClasses/BaseSpiceActivity.java
+++ MuSeek/app/src/main/java/com/letb/museek/BaseClasses/BaseSpiceActivity.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
+import com.letb.museek.R;
 import com.letb.museek.Services.SpiceRequestService;
 import com.octo.android.robospice.SpiceManager;
 
@@ -13,7 +17,7 @@ import com.octo.android.robospice.SpiceManager;
 // В джаве экстендить можно только один класс, поэтому придется экстендить рано
 // Не работает пока меню не используется
 //public abstract class BaseSpiceActivity extends SlidingActivity {
-public abstract class BaseSpiceActivity extends FragmentActivity {
+public abstract class BaseSpiceActivity extends AppCompatActivity  {
 
     private SpiceManager spiceManager = new SpiceManager( SpiceRequestService.class );
 
@@ -27,6 +31,33 @@ public abstract class BaseSpiceActivity extends FragmentActivity {
     protected void onStop() {
         spiceManager.shouldStop();
         super.onStop();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // User chose the "Settings" item, show the app settings UI...
+                return true;
+
+            case R.id.action_favorite:
+                // User chose the "Favorite" action, mark the current item
+                // as a favorite...
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
     }
 
     protected SpiceManager getSpiceManager() {
--- MuSeek/app/src/main/java/com/letb/museek/MainActivity.java
+++ MuSeek/app/src/main/java/com/letb/museek/MainActivity.java
@@ -1,17 +1,18 @@
 package com.letb.museek;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-import com.letb.museek.BaseClasses.BaseSpiceActivity;
 import com.letb.museek.Fragments.ArtistListFragment;
 import com.letb.museek.Fragments.PlaylistFragment;
 import com.letb.museek.Models.Artist;
-import com.letb.museek.Models.Track.Track;
 import com.letb.museek.Utils.UserInformer;
 
 import java.util.List;
@@ -19,7 +20,6 @@ import java.util.List;
 public class MainActivity extends PlaylistActivity implements ArtistListFragment.OnArtistSelectedListener {
 
     private List<Artist> artistList;
-    private List<Track>  trackList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +28,25 @@ public class MainActivity extends PlaylistActivity implements ArtistListFragment
         setContentView(R.layout.activity_main);
 
         artistList = (List<Artist>) getIntent().getExtras().getSerializable(ArtistListFragment.ARTIST_LIST);
-        trackList =  (List<Track>) getIntent().getExtras().getSerializable(PlaylistFragment.TRACK_LIST);
 
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         showFragment(new ArtistListFragment(), getIntent(), R.id.artist_container, ft);
         showFragment(new PlaylistFragment(), getIntent(), R.id.track_container, ft);
         ft.commit();
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+//        myToolbar.setNavigationIcon(R.drawable.icon_play);
+//        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                NavUtils.navigateUpFromSameTask(MainActivity.this);
+//            }
+//        });
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Music");
     }
 
     @Override
